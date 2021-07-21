0da56d70ce801d557bdf8ed5d4ea65787bde135a
Zain Jakwani <zjakwani@gmail.com>
2021-07-21 16:37:10 -0700

    heroku change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import Api, Resource, reqparse
 from api.ApiHandler import ApiHandler
 
 app = Flask(__name__, static_url_path="", static_folder="frontend/build")
-CORS(app)  # comment this on deployment
+# CORS(app)  # comment this on deployment
 api = Api(app)
 
 
