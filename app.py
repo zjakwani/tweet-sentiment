f2481163ce741780cf5024e20df0659f43fed630
Zain Jakwani <zjakwani@gmail.com>
2021-07-26 09:37:55 -0700

    fix gunicorn

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 
-# from flask_cors import CORS  # comment this on deployment
+from flask_cors import CORS  # comment this on deployment
 from api.ApiHandler import ApiHandler
 
 app = Flask(__name__)
-# CORS(app)  # comment this on deployment
+CORS(app)  # comment this on deployment
 api = Api(app)
 
 
