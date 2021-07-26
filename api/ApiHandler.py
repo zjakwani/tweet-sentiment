4069ab213c46d0db66e967c6a9dff554b9f6f944
Zain Jakwani <zjakwani@gmail.com>
2021-07-26 09:44:29 -0700

    test 2

--- api/ApiHandler.py
+++ api/ApiHandler.py
@@ -7,9 +7,17 @@ class ApiHandler(Resource):
         return {"resultStatus": "SUCCESS", "message": "Hello Api Handle"}
 
     def post(self):
+        consumerKey = process.env.consumerKey
+        consumerSecret = process.env.consumerSecret
+        accessToken = process.env.accessToken
+        accessTokenSecret = process.env.accessTokenSecret
+        auth = tweepy.OAuthHandler(
+            consumer_key=consumerKey, consumer_secret=consumerSecret
+        )
+        auth.set_access_token(accessToken, accessTokenSecret)
         parser = reqparse.RequestParser()
-        # parser.add_argument("keyword", type=str)
-        # parser.add_argument("n", type=str)
+        parser.add_argument("keyword", type=str)
+        parser.add_argument("n", type=str)
         parser.add_argument("message", type=str)
 
         args = parser.parse_args()
@@ -24,14 +32,6 @@ class ApiHandler(Resource):
 
         final_ret = {"status": "Success", "message": message}
 
-        # consumerKey = process.env.consumerKey
-        # consumerSecret = process.env.consumerSecret
-        # accessToken = process.env.accessToken
-        # accessTokenSecret = process.env.accessTokenSecret
-        # auth = tweepy.OAuthHandler(
-        #     consumer_key=consumerKey, consumer_secret=consumerSecret
-        # )
-        # auth.set_access_token(accessToken, accessTokenSecret)
         # api = tweepy.API(auth)
 
         # search = args["keyword"]
