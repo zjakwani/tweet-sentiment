06ae25172f16222a02ae102565091fa9e1c4f276
Zain Jakwani <zjakwani@gmail.com>
2021-07-26 09:25:40 -0700

    integrate twitter api

--- api/ApiHandler.py
+++ api/ApiHandler.py
@@ -1,4 +1,5 @@
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Resource, reqparse
+import tweepy
 
 
 class ApiHandler(Resource):
@@ -6,28 +7,48 @@ class ApiHandler(Resource):
         return {"resultStatus": "SUCCESS", "message": "Hello Api Handle"}
 
     def post(self):
-        print(self)
         parser = reqparse.RequestParser()
-        parser.add_argument("type", type=str)
-        parser.add_argument("message", type=str)
+        parser.add_argument("keyword", type=str)
+        parser.add_argument("n", type=str)
 
         args = parser.parse_args()
 
         print(args)
         # note, the post req from frontend needs to match the strings here (e.g. 'type and 'message')
 
-        request_type = args["type"]
-        request_json = args["message"]
-        # ret_status, ret_msg = ReturnData(request_type, request_json)
-        # currently just returning the req straight
-        ret_status = request_type
-        ret_msg = request_json
+        # request_type = args["type"]
+        # request_json = args["message"]
+        # # ret_status, ret_msg = ReturnData(request_type, request_json)
+        # # currently just returning the req straight
+        # ret_status = request_type
+        # ret_msg = request_json
 
-        if ret_msg:
-            message = "Your Message Requested: {}".format(ret_msg)
-        else:
-            message = "No Msg"
+        # if ret_msg:
+        #     message = "Your Message Requested: {}".format(ret_msg)
+        # else:
+        #     message = "No Msg"
 
-        final_ret = {"status": "Success", "message": message}
+        # final_ret = {"status": "Success", "message": message}
+
+        consumerKey = process.env.consumerKey
+        consumerSecret = process.env.consumerSecret
+        accessToken = process.env.accessToken
+        accessTokenSecret = process.env.accessTokenSecret
+        auth = tweepy.OAuthHandler(
+            consumer_key=consumerKey, consumer_secret=consumerSecret
+        )
+        auth.set_access_token(accessToken, accessTokenSecret)
+        api = tweepy.API(auth)
+
+        search = args["keyword"]
+        n = int(args["n"])
+
+        tweets = tweepy.Cursor(api.search, q=search, result_type="recent").items(n)
+
+        tweetarr = []
+        for t in tweets:
+            tweetarr.append({"handle": t.user.screen_name, "text": t.text})
+
+        final_ret = tweetarr
 
         return final_ret
--- app.py
+++ app.py
@@ -1,22 +1,12 @@
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
+from flask import Flask
+from flask_restful import Api
 
 # from flask_cors import CORS  # comment this on deployment
 from api.ApiHandler import ApiHandler
 
-app = Flask(__name__, static_url_path="", static_folder="frontend/build")
+app = Flask(__name__)
 # CORS(app)  # comment this on deployment
 api = Api(app)
-
-
-@app.route("/", defaults={"path": ""})
-def serve(path):
-    return send_from_directory(app.static_folder, "index.html")
-
-
-@app.errorhandler(404)
-def not_found(e):
-    return send_from_directory(app.static_folder, "index.html")
 
 
 api.add_resource(ApiHandler, "/flask/hello")
