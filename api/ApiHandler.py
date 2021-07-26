b2c4e777fcf7a8dcb00e7af0185197f7eba7f6bb
Zain Jakwani <zjakwani@gmail.com>
2021-07-26 14:05:09 -0700

    integrate textblob

--- api/ApiHandler.py
+++ api/ApiHandler.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource, reqparse
 import tweepy
 import os
+from textblob import TextBlob
 
 
 class ApiHandler(Resource):
@@ -20,30 +21,35 @@ class ApiHandler(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("keyword", type=str)
         parser.add_argument("n", type=str)
-        parser.add_argument("message", type=str)
-
         args = parser.parse_args()
 
-        # ret_msg = args["message"]
-
-        # if ret_msg:
-        #     message = "Your Message Requested: {}".format(ret_msg)
-        # else:
-        #     message = "No Msg"
-
-        # final_ret = {"status": "Success", "message": consumerKey}
-
         api = tweepy.API(auth)
-
         search = args["keyword"]
         n = int(args["n"])
 
-        tweets = tweepy.Cursor(api.search, q=search, result_type="recent").items(n)
+        tweets = tweepy.Cursor(
+            api.search, q=search, result_type="recent", lang="en"
+        ).items(n)
 
         tweetarr = []
         for t in tweets:
-            tweetarr.append({"handle": t.user.screen_name, "text": t.text})
+            blob = TextBlob(t.text)
+            polarity = blob.sentiment.polarity
+            sentiment = ""
+            if polarity > 0:
+                sentiment = "positive"
+            elif polarity < 0:
+                sentiment = "negative"
+            else:
+                sentiment = "neutral"
+            tweetarr.append(
+                {
+                    "handle": t.user.screen_name,
+                    "text": t.text,
+                    "polarity": polarity,
+                    "sentiment": sentiment,
+                    "words": blob.words[0],
+                }
+            )
 
-        final_ret = tweetarr
-
-        return final_ret
+        return tweetarr
